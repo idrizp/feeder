8acc041d45fabf5323e863de3e0d680299621df3
Idriz Pelaj <idrizpelaj@hotmail.com>
2025-01-02 12:51:43 +0100

    Handle switching book-keeping server-side so falsifications don't happen.

--- service/src/main/java/dev/idriz/feeder/ws/channel/SwitchWebSocketChannel.java
+++ service/src/main/java/dev/idriz/feeder/ws/channel/SwitchWebSocketChannel.java
@@ -7,17 +7,22 @@ import dev.idriz.feeder.ws.WebSocketStatus;
 import io.javalin.websocket.WsMessageContext;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SwitchWebSocketChannel implements WebSocketChannel<SwitchWebSocketChannel.ViewWebSocketPayload> {
 
     private final KafkaManager kafkaManager;
     private final SentryManager sentryManager;
+    private final Map<String, String> currentPages = new ConcurrentHashMap<>();
+    private final Map<String, Long> lastPageSwitches = new ConcurrentHashMap<>();
 
     public SwitchWebSocketChannel(final @NotNull KafkaManager kafkaManager, final @NotNull SentryManager sentryManager) {
         this.kafkaManager = kafkaManager;
         this.sentryManager = sentryManager;
     }
 
-    public record ViewWebSocketPayload(@NotNull String origin, @NotNull String destination, long spent) {
+    public record ViewWebSocketPayload(@NotNull String destination, long spent) {
     }
 
     @NotNull
@@ -35,15 +40,26 @@ public class SwitchWebSocketChannel implements WebSocketChannel<SwitchWebSocketC
     @NotNull
     @Override
     public WebSocketStatus onMessageReceived(final @NotNull WsMessageContext context, final @NotNull ViewWebSocketPayload data) {
-        String url = data.origin();
-        long spent = data.spent();
-        if (spent < 0) {
-            sentryManager.logException(
-                    new IllegalArgumentException("Invalid switch message: " + data)
-            );
-            return WebSocketStatus.INVALID_INITIAL_PAYLOAD;
+        String destination = data.destination();
+
+        long now = System.currentTimeMillis();
+        long spent = lastPageSwitches.getOrDefault(context.sessionId(), now);
+        long delta = now - spent;
+
+        if (delta == 0) {
+            // We don't want to log this, because we haven't actually switched pages.
+            return WebSocketStatus.OK;
         }
-        kafkaManager.publish("switch", url + "|" + data.destination() + "|" + spent);
+
+        lastPageSwitches.put(context.sessionId(), now);
+        String previous = currentPages.put(context.sessionId(), destination);
+
+        if (previous == null) {
+            // This is the first time we've seen this session.
+            return WebSocketStatus.OK;
+        }
+
+        kafkaManager.publish("switch", previous + "|" + destination + "|" + spent);
         return WebSocketStatus.OK;
     }
 }
